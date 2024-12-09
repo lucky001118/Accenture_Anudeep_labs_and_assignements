3bdc8d438bd05beb3e2487690ee29223d8624f6e
lucky001118 <manikpurilucky218@gmail.com>
2024-12-09 22:51:17 +0530

    update the repository

---
+++ src/JDBC/theory.java
@@ -0,0 +1,12 @@
+package JDBC;
+
+public class theory {
+//    To connect Java application with the MySQL database, we need to follow 5 following steps.
+
+    /*
+Driver class: The driver class for the mysql database is com.mysql.jdbc.Driver.
+Connection URL: The connection URL for the mysql database is jdbc:mysql://localhost:3306/sonoo where jdbc is the API, mysql is the database, localhost is the server name on which mysql is running, we may also use IP address, 3306 is the port number and sonoo is the database name. We may use any database, in such case, we need to replace the sonoo with our database name.
+Username: The default username for the mysql database is root.
+Password: It is the password given by the user at the time of installing the mysql database. In this example, we are going to use root as the password.
+     */
+}
