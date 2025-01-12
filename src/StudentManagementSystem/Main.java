8b21ba0d1bb19f6c0386b77e6f68098c02d2b770
Lucky Manikpuri <149005970+lucky001118@users.noreply.github.com>
2025-01-12 22:55:43 +0530

    Update Main.java

--- src/StudentManagementSystem/Main.java
+++ src/StudentManagementSystem/Main.java
@@ -4,6 +4,8 @@ import StudentManagementSystem.useCase.StudentAllOperation;
 
 public class Main {
     public static void main(String[] args) {
+
+        // here all the students all opearations are presents
         StudentAllOperation stusentAllOperation = new StudentAllOperation();
 
         stusentAllOperation.allOps();
